633dd9828512ead79a90c7c914effea9cd562350
unknown <oakhmadjonov1@gmail.com>
2023-05-06 15:07:09 +0500

    Footer is done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import "./Home.scss";
 import Banner from "./components/Banner/Banner";
 import Recap from "./components/Recap/Recap";
+import Footer from "../../components/Footer/Footer";
 const Home = () => {
   return (
     <div className="home">
       <Banner />
       <Recap />
+
+      <Footer />
     </div>
   );
 };
