624f694a116a48048d0b678f57516000337b96d5
unknown <oakhmadjonov1@gmail.com>
2023-05-06 17:03:04 +0500

    Home page almost done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,12 +3,13 @@ import "./Home.scss";
 import Banner from "./components/Banner/Banner";
 import Recap from "./components/Recap/Recap";
 import Footer from "../../components/Footer/Footer";
+import Members from "./components/Members/Members";
 const Home = () => {
   return (
     <div className="home">
       <Banner />
       <Recap />
-
+      <Members />
       <Footer />
     </div>
   );
