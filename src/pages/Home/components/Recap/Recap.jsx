89ee4706292b06c0e064f7806c5b2540b0a6241a
unknown <oakhmadjonov1@gmail.com>
2023-05-06 12:12:05 +0500

    Header and banner done

---
+++ src/pages/Home/components/Recap/Recap.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Recap.scss";
+const Recap = () => {
+  return (
+    <div className="recap">
+      <div className="recap__container">
+        <div className="recap__container-wrapper"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Recap;
