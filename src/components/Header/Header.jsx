5b2a3773a9b0adbb456133483063884f224a210d
unknown <oakhmadjonov1@gmail.com>
2023-05-08 08:39:25 +0500

    Menu function added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
 import "./Header.scss";
 const Header = () => {
+  const [count, setCount] = useState(1);
+
+  let step1 = false,
+    step2 = false,
+    step3 = false,
+    step4 = false,
+    step5 = false;
+
+  switch (count) {
+    case 1:
+      step1 = true;
+      break;
+    case 2:
+      step2 = true;
+      break;
+    case 3:
+      step3 = true;
+      break;
+    case 4:
+      step4 = true;
+      break;
+    case 5:
+      step5 = true;
+      break;
+
+    default:
+      step1 = true;
+  }
+
   return (
     <div className="header">
       <div className="header__container">
@@ -13,15 +42,32 @@ const Header = () => {
             </Link>
           </div>
           <div className="menu">
-            <Link to="/" className="menu__link">
+            <Link
+              to="/"
+              className={step1 ? "active" : "menu__link"}
+              onClick={() => setCount(1)}>
               HOME
             </Link>
-            <Link to={"/Event"} className="menu__link">
+            <Link
+              to={"/Event"}
+              className={step2 ? "active" : "menu__link"}
+              onClick={() => setCount(2)}>
               WPL EVENT #2, 4/20
             </Link>
-            <Link to={'/Players'} className="menu__link">PLAYERS</Link>
-            <Link className="menu__link">PAST RESULTS</Link>
-            <Link className="menu__link">
+            <Link
+              to={"/Players"}
+              className={step3 ? "active" : "menu__link"}
+              onClick={() => setCount(3)}>
+              PLAYERS
+            </Link>
+            <Link
+              className={step4 ? "active" : "menu__link"}
+              onClick={() => setCount(4)}>
+              PAST RESULTS
+            </Link>
+            <Link
+              className={step5 ? "active" : "menu__link"}
+              onClick={() => setCount(5)}>
               <div className="live_stream">LIVE STREAM</div>
             </Link>
           </div>
